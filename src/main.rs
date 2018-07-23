1a8ddae2c698b1c6add6c7663f161598c3198d09
Olaf Michaelis <omichaelis@aip.de>
2018-07-23 13:04:11 +0200

    Add first try of http client request

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@ extern crate serde_derive;
 extern crate serde_json;
 
 // my packages
+mod requester;
+
 mod services_file_reader;
 use  services_file_reader::IP;
 
 
 fn main() {
-    println!("{:?}", "Running application...");
+    println!("{:#?}", "Running application...");
 
     let ip = IP::init();
-    println!("{:?}",ip);
+    println!("{:#?}",ip);
+
+    let rs = requester::get_url("http://www.google.com");
+    // print!("{:?}", rs);
 }
